24a53745746b45ad5ba2defcb90179416a2b716e
Mikhail Paymetov <muxa147@gmail.com>
2020-10-08 02:47:24 +0300

    добавил обработку окна, изменение размера и эффекта

--- js/main.js
+++ js/main.js
@@ -14,6 +14,11 @@ const MESSAGES = [
   "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
   "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
 ];
+const EFFECT_CLASS_START = 'effects__preview--';
+const MAX_CONTROL_VALUE = 100;
+const MIN_CONTROL_VALUE = 25;
+const CONTROL_STEP = 25;
+const HIDE_CLASS = 'hidden';
 
 const getRandom = function (number = 1, offset = 0) {
   const result = Math.round(number * Math.random() + offset);
@@ -94,3 +99,163 @@ const renderPictures = function () {
 };
 
 renderPictures();
+
+const fileInput = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const modalCloseButton = document.querySelector('#upload-cancel');
+
+const onModalEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const openModal = function () {
+  imgUploadOverlay.classList.remove(HIDE_CLASS);
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalEscPress);
+};
+
+const closeModal = function () {
+  imgUploadOverlay.classList.add(HIDE_CLASS);
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onModalEscPress);
+};
+
+fileInput.addEventListener('change', function () {
+  openModal();
+});
+
+modalCloseButton.addEventListener('click', function () {
+  closeModal();
+});
+
+openModal();
+
+const controlSmallerButton = document.querySelector('.scale__control--smaller');
+const controlBiggerButton = document.querySelector('.scale__control--bigger');
+const controlValue = document.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview');
+
+const getIntValue = function (element) {
+  const valueString = element.value;
+  let number = window.parseInt(valueString);
+  return number;
+};
+
+const setScale = function (element, percent) {
+  const scaleValue = percent / 100;
+  element.style = `transform: scale(${scaleValue})`;
+};
+
+const valueUp = function () {
+  let number = getIntValue(controlValue);
+  if ((number + CONTROL_STEP) <= MAX_CONTROL_VALUE) {
+    number += CONTROL_STEP;
+    controlValue.value = number + '%';
+    setScale(imagePreview, number);
+  }
+};
+
+const valueDown = function () {
+  let number = getIntValue(controlValue);
+  if ((number - CONTROL_STEP) >= MIN_CONTROL_VALUE) {
+    number -= CONTROL_STEP;
+    controlValue.value = number + '%';
+    setScale(imagePreview, number);
+  }
+};
+
+controlSmallerButton.addEventListener('click', function () {
+  valueDown();
+});
+
+controlBiggerButton.addEventListener('click', function () {
+  valueUp();
+});
+
+const form = document.querySelector('.img-upload__form');
+
+const getEffectList = function () {
+  let effectsList = [];
+  const effectButtons = document.querySelectorAll('.effects__radio');
+  for (let i = 0; i < effectButtons.length; i++) {
+    const effect = effectButtons[i].value;
+    effectsList.push(effect);
+  }
+  return effectsList;
+};
+
+const effectsList = getEffectList();
+
+const removeAllEffectClasses = function () {
+  for (let i = 0; i < effectsList.length; i++) {
+    const effectClassName = EFFECT_CLASS_START + effectsList[i];
+    if (imagePreview.classList.contains(effectClassName)) {
+      imagePreview.classList.remove(effectClassName);
+    }
+  }
+};
+
+const effectLevelBar = document.querySelector('.img-upload__effect-level');
+let imageEffect = 'none';
+
+const onEffectsChange = function (evt) {
+  if (evt.target && evt.target.matches('input[type="radio"]')) {
+    removeAllEffectClasses();
+    imagePreview.style = '';
+    imageEffect = evt.target.value;
+    if (imageEffect !== 'none') {
+      const effectClass = EFFECT_CLASS_START + imageEffect;
+      imagePreview.classList.add(effectClass);
+      if (effectLevelBar.classList.contains(HIDE_CLASS)) {
+        effectLevelBar.classList.remove(HIDE_CLASS);
+      }
+    } else {
+      effectLevelBar.classList.add(HIDE_CLASS);
+    }
+  }
+};
+
+form.addEventListener('change', onEffectsChange);
+
+const effectLevelPin = document.querySelector('.effect-level__pin');
+const effectLevelLine = document.querySelector('.effect-level__line');
+const effectLevelDepth = document.querySelector('.effect-level__depth');
+const effectLevelValue = document.querySelector('.effect-level__value');
+
+const setEffectStyle = function (level) {
+  const levelPercent = level * 100;
+  let effectStyle = '';
+  if (imageEffect === 'chrome') {
+    effectStyle = 'filter: grayscale(' + level + ')';
+  }
+  if (imageEffect === 'sepia') {
+    effectStyle = 'filter: sepia(' + level + ')';
+  }
+  if (imageEffect === 'marvin') {
+    effectStyle = 'filter: invert(' + levelPercent + '%)';
+  }
+  if (imageEffect === 'phobos') {
+    const effectValue = level * 3;
+    effectStyle = 'filter: blur(' + effectValue + 'px)';
+  }
+  if (imageEffect === 'heat') {
+    const effectValue = level * 2 + 1;
+    effectStyle = 'filter: brightness(' + effectValue + ')';
+  }
+  imagePreview.style = effectStyle;
+};
+
+const onPinMove = function () {
+  const effectLevelLineWidth = effectLevelLine.offsetWidth;
+  const effectLevelDepthWidth = effectLevelDepth.offsetWidth;
+  const level = (effectLevelDepthWidth / effectLevelLineWidth).toFixed(2);
+  const levelPercent = level * 100;
+  effectLevelValue.value = levelPercent;
+  setEffectStyle(level);
+};
+
+effectLevelPin.addEventListener('mouseup', onPinMove);
